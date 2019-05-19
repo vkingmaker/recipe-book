4d49f5cb0219b2744981185bdc7a1e08cd405ed1
Monday Victor A <vkingmaker1@gmail.com>
2019-05-19 14:15:37 +0100

    feature(recipePropertys) adds the necessary propertys
    This commit adds the necessary propertys relevant to the project
    
    [Delivers #recipePropertys]

--- classes/Recipe.php
+++ classes/Recipe.php
@@ -1,9 +1,18 @@
 <?php
 
 class Recipe {
+    public $title;
+    public $ingredients = array();
+    public $instructions = array();
+    public $yield;
+    public $tag = array();
+    public $source = 'Monday Victor';
 
 }
 
 $recipe1 = new Recipe();
+$recipe2 = new Recipe();
+$recipe2->source = 'Monday Akubudike';
 
-var_dump($recipe1);
+var_dump($recipe1->source);
+var_dump($recipe2->source);
