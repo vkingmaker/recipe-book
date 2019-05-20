1d7242ed2fa5e02acc73522911dcd3362b8dce24
Monday Victor A <vkingmaker1@gmail.com>
2019-05-20 12:51:30 +0100

    feature(encapsulation): adds private properties and public methods
    This commit uses private propertys and a pubic getter and setter to manipulate the values
    
    [Delivers encapsulation]

--- classes/Recipe.php
+++ classes/Recipe.php
@@ -1,18 +1,38 @@
 <?php
 
 class Recipe {
-    public $title;
+    private $title;
     public $ingredients = array();
     public $instructions = array();
     public $yield;
     public $tag = array();
     public $source = 'Monday Victor';
 
+    public function displayTitle()
+    {
+        return "{$this->title} by {$this->source}";
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = ucwords($title);
+    }
+
 }
 
 $recipe1 = new Recipe();
-$recipe2 = new Recipe();
-$recipe2->source = 'Monday Akubudike';
+$recipe1->source = 'Otti Grant';
+$recipe1->setTitle('Baked beans');
+echo $recipe1->getTitle();
 
-var_dump($recipe1->source);
-var_dump($recipe2->source);
+$recipe2 = new Recipe();
+$recipe2->source = 'Akubudike Monday';
+$recipe2->setTitle('coconut Rice');
+
+echo $recipe1->displayTitle();
+echo $recipe2->displayTitle();
